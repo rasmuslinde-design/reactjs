cc8df86953cbd7dd993d4e26907b2cb8046d114c
rasmus <rasmus.linde@voco.ee>
2025-12-10 11:41:52 +0200

    teinepush

---
+++ React projekt/src/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header>
+      <h1>Lakers in 5</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
