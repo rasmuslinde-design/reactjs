1b561c96cc1d0b604c456815da406d37faf87c99
rasmus <rasmus.linde@voco.ee>
2026-02-02 19:14:21 +0200

    ylesanne6

--- React projekt/src/Newexpenses/ExpenseForm.jsx
+++ React projekt/src/Newexpenses/ExpenseForm.jsx
@@ -1,85 +1,70 @@
-import "./ExpenseForm.css";
-import { useState } from "react";
+import React, { useState } from 'react';
+import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredPrice: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
-  const titleHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
-  };
-
-  const amountHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredPrice: event.target.value };
-    });
-  };
-
-  const dateHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredDate: event.target.value };
-    });
-  };
+  const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
+  const amountChangeHandler = (event) => setEnteredAmount(event.target.value);
+  const dateChangeHandler = (event) => setEnteredDate(event.target.value);
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     const expenseData = {
-      title: userInput.enteredTitle,
-      price: userInput.enteredPrice,
-      date: new Date(userInput.enteredDate),
+      title: enteredTitle,
+      price: +enteredAmount, // Muudan numbriks
+      date: new Date(enteredDate),
     };
-  
-    console.log(expenseData);
 
     props.onSaveExpenseData(expenseData);
-
-    setUserInput({
-      enteredTitle: "",
-      enteredPrice: "",
-      enteredDate: "",
-    });
+    
+    // vormi tühjenuds
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
           <label>Title</label>
           <input 
-            type="text" 
-            onChange={titleHandler}
-            value={userInput.enteredTitle} 
+            type='text' 
+            value={enteredTitle} 
+            onChange={titleChangeHandler} 
+            required 
           />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            onChange={amountHandler}
-            value={userInput.enteredPrice}
+          <input 
+            type='number' 
+            min='0.01' 
+            step='0.01' 
+            value={enteredAmount} 
+            onChange={amountChangeHandler} 
+            required 
           />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Date</label>
-          <input
-            type="date"
-            min="2022-01-01"
-            max="2025-12-31"
-            onChange={dateHandler}
-            value={userInput.enteredDate}
+          <input 
+            type='date' 
+            min='2023-01-01' 
+            max='2026-12-31' 
+            value={enteredDate} 
+            onChange={dateChangeHandler} 
+            required 
           />
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
--- React projekt/src/Newexpenses/NewExpense.jsx
+++ React projekt/src/Newexpenses/NewExpense.jsx
@@ -1,17 +1,49 @@
-import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react';
+import ExpenseForm from './ExpenseForm';
+// import './NewExpense.css';
 
 const NewExpense = (props) => {
+  // Vihje 1? editForm olek (isEditing)
+  const [isEditing, setIsEditing] = useState(false);
+
+  // vormi avamine
+  const startEditingHandler = () => {
+    setIsEditing(true);
+    console.log("form edit open");
+  };
+
+  // cancel nupp
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+    console.log("form edit close");
+  };
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
+    
+    // app.jsx saab andmed
     props.onAddExpense(expenseData);
+    
+    // Vihje 4? Pane vorm kinni pärast lisamist
+    setIsEditing(false);
+    console.log("form edit close");
   };
 
   return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className='new-expense'>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      
+      {isEditing && (
+        <ExpenseForm 
+          onSaveExpenseData={saveExpenseDataHandler} 
+          onCancel={stopEditingHandler} 
+        />
+      )}
     </div>
   );
 };
